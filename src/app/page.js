b8a7c3de7f564d88366f357213c086a81caed15a
skmtrd <skmtgame23@gmail.com>
2024-09-18 12:18:23 +0900

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,31 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return <div className={styles.container}>test!!</div>;
+  const [health, setHealth] = useState("");
+
+  useEffect(() => {
+    const fetchHealth = async () => {
+      try {
+        const res = await fetch("/api/health");
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        const data = await res.json();
+        console.log(data);
+        setHealth(data.message);
+      } catch (error) {
+        console.error(error);
+        setHealth("エラー: APIからのデータ取得に失敗しました");
+      }
+    };
+    fetchHealth();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1>{health}</h1>
+    </div>
+  );
 }
