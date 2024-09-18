4e606ffd419512b2839ce88eb0c42d65d9e69475
skmtrd <skmtgame23@gmail.com>
2024-09-18 12:03:17 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/your-repo-name" : "",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/githubpages-deploy-test" : "",
   images: {
     unoptimized: true,
   },
