acde8dd89a9b585cfc4f62694453958785486f4e
skmtrd <skmtgame23@gmail.com>
2024-09-19 14:27:33 +0900

    homeページ途中

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
 'use client';
 import styles from './page.module.css';
+import Image from 'next/image';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.text}>Hello World!!!</h1>
+      <Image
+        className={styles.IconImage}
+        src="/images/1.png"
+        alt="image"
+        width={165}
+        height={165}
+      />
+      <video width="640" height="360" autoPlay loop muted playsInline>
+        <source src="/videos/Video.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
