b269e42d9a9355fb66253789a502698e8d7487e1
skmtrd <skmtgame23@gmail.com>
2024-09-19 15:57:43 +0900

    homeページ完成

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,26 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Image
-        className={styles.IconImage}
-        src="/images/1.png"
-        alt="image"
-        width={165}
-        height={165}
-      />
-      <video width="640" height="360" autoPlay loop muted playsInline>
-        <source src="/videos/Video.mp4" type="video/mp4" />
-      </video>
+      <div className={styles.main}>
+        <div className={styles.IntroContainer}>
+          <Image
+            className={styles.IconImage}
+            src="/images/1.png"
+            alt="image"
+            width={165}
+            height={165}
+          />
+          <div className={styles.IntroTextContainer}>
+            <div className={styles.IntroText}>Video Creator</div>
+            <div className={styles.IntroText}>みつば</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.video}>
+        <video autoPlay loop muted playsInline>
+          <source src="/videos/Video.mp4" type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 }
