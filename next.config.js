d48ba0ddc7e41e93978ccadc4d3b258b468cd671
skmtrd <skmtgame23@gmail.com>
2024-09-19 13:02:17 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/3itsu8.github.io' : '',
-  assetPrefix:
-    process.env.NODE_ENV === 'production' ? '/3itsu8.github.io/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
   images: {
     unoptimized: true,
   },
