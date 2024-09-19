5abdbd08a0a551f8c7661fa2e8a88c0e25d1a0e9
skmtrd <skmtgame23@gmail.com>
2024-09-19 13:05:07 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  assetPrefix: './',
   images: {
     unoptimized: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
