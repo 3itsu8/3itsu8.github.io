672bac8ccd5ff8738d08ac503e5d24bf334531d4
skmtrd <skmtgame23@gmail.com>
2024-09-19 12:18:12 +0900

    test

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello World</h1>
+      <h1 className={styles.text}>Hello World!!!</h1>
     </div>
   );
 }
