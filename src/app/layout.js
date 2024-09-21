d0e84b5296ce5a8ac4993a4b5d09e6e036276c73
みつば <mitsuba.mov@gmail.com>
2024-09-21 15:00:16 +0900

    Framer Motionを入れた
    よくわかりませんでした

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={`${LINESeedJP_OTF_Bd.variable}`}>
         <Header />
         {children}
