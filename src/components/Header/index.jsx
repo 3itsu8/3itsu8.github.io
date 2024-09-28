7e294ccc848bb9f94c484afd265b8d613cd092a1
みつば <mitsuba.mov@gmail.com>
2024-09-28 10:57:36 +0900

    ハンバーガーメニューの作成
    完成途中です

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,26 @@
 import React, { useState } from 'react';
 import styles from './Header.module.css';
 import Link from 'next/link';
+// import { motion } from 'framer-motion';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
+
+  const toggleMenu = () => {
+    setOpen(!open);
+    console.log('sample');
+  };
+
+  const navLinks = [
+    { title: 'Home', href: '/' },
+    { title: 'Projects', href: '/projects' },
+    { title: 'About', href: '/about' },
+  ];
 
   return (
     <header className={styles.header}>
@@ -30,7 +43,13 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-        <button onClick={handleClick} className={styles.button}>
+        <button
+          onClick={() => {
+            handleClick();
+            toggleMenu();
+          }}
+          className={styles.button}
+        >
           <span
             className={`${styles.span} ${isOpen ? styles.openFirst : styles.closedFirst}`}
           ></span>
@@ -41,9 +60,33 @@ const Header = () => {
             className={`${styles.span} ${isOpen ? styles.openThird : styles.closedThird}`}
           ></span>
         </button>
+        {open && (
+          <div className={styles.mobile_nav_background}>
+            <div className={styles.mobile_nav}>
+              {navLinks.map((link, index) => {
+                return (
+                  <MobileNavLink
+                    className={styles.title_links}
+                    key={index}
+                    title={link.title}
+                    href={link.href}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        )}
       </nav>
     </header>
   );
 };
 
 export default Header;
+
+const MobileNavLink = ({ title, href }) => {
+  return (
+    <div>
+      <Link href={href}>{title}</Link>
+    </div>
+  );
+};
