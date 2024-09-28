eac31b53bdf3f7105856410ff309509288703a38
みつば <mitsuba.mov@gmail.com>
2024-09-28 12:16:30 +0900

    ハンバーガーメニューの機能の実装
    基本的な機能を実装しました

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,19 +2,50 @@
 import React, { useState } from 'react';
 import styles from './Header.module.css';
 import Link from 'next/link';
-// import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const [open, setOpen] = useState(false);
-
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
 
   const toggleMenu = () => {
     setOpen(!open);
-    console.log('sample');
+  };
+
+  const menuVars = {
+    initial: {
+      scaleY: 0,
+    },
+    animate: {
+      scaleY: 1,
+      transition: {
+        duration: 0.5,
+        ease: [0.12, 0, 0.39, 0],
+      },
+    },
+    exit: {
+      scaleY: 0,
+      transition: {
+        delay: 0.2,
+        duration: 0.5,
+        ease: [0.39, 1, 0.4, 1],
+      },
+    },
+  };
+
+  const containerVars = {
+    initial: {
+      transition: {
+        staggerChildren: 0.09,
+        staggerDirection: -1,
+      },
+    },
+    open: {
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.09,
+        staggerDirection: 1,
+      },
+    },
   };
 
   const navLinks = [
@@ -43,39 +74,48 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-        <button
-          onClick={() => {
-            handleClick();
-            toggleMenu();
-          }}
-          className={styles.button}
-        >
+        <button onClick={toggleMenu} className={styles.button}>
           <span
-            className={`${styles.span} ${isOpen ? styles.openFirst : styles.closedFirst}`}
+            className={`${styles.span} ${open ? styles.openFirst : styles.closedFirst}`}
           ></span>
           <span
-            className={`${styles.span} ${isOpen ? styles.openSecond : styles.closedSecond}`}
+            className={`${styles.span} ${open ? styles.openSecond : styles.closedSecond}`}
           ></span>
           <span
-            className={`${styles.span} ${isOpen ? styles.openThird : styles.closedThird}`}
+            className={`${styles.span} ${open ? styles.openThird : styles.closedThird}`}
           ></span>
         </button>
-        {open && (
-          <div className={styles.mobile_nav_background}>
-            <div className={styles.mobile_nav}>
-              {navLinks.map((link, index) => {
-                return (
-                  <MobileNavLink
-                    className={styles.title_links}
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              variants={menuVars}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              className={styles.mobile_nav_background}
+            >
+              <motion.div
+                variants={containerVars}
+                initial="initial"
+                animate="open"
+                exit="initial"
+                className={styles.mobile_nav}
+              >
+                {navLinks.map((link, index) => (
+                  <motion.div
                     key={index}
-                    title={link.title}
-                    href={link.href}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        )}
+                    variants={mobileLinkVars}
+                    className="overflow-hidden"
+                  >
+                    <Link href={link.href}>
+                      <span onClick={toggleMenu}>{link.title}</span>
+                    </Link>
+                  </motion.div>
+                ))}
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </nav>
     </header>
   );
@@ -83,10 +123,20 @@ const Header = () => {
 
 export default Header;
 
-const MobileNavLink = ({ title, href }) => {
-  return (
-    <div>
-      <Link href={href}>{title}</Link>
-    </div>
-  );
+const mobileLinkVars = {
+  initial: {
+    y: '-70%',
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: [0, 0.55, 0.45, 1],
+    },
+  },
 };
