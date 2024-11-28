b3e5c7ce9318ddd116fbbf788c5ec28522ce7fe1
skmtrd <skmtgame23@gmail.com>
2024-11-28 11:18:51 +0900

    オセロの調整

--- src/app/projects/Othello/page.jsx
+++ src/app/projects/Othello/page.jsx
@@ -13,36 +13,38 @@ const Page = () => {
           <a href="/projects"> &lt;All Projects</a>
         </div>
         <div className={styles.main_info}>
-          <div className={styles.video_link}>
-            <Image
-              src="/images/project/2024_Othello.png"
-              alt='映像"Othello"'
-              width={960}
-              height={540}
-            />
-            <LinkButton link="https://x.com/3itsu8/status/1822951166063554767">
-              X&#40;Twitter&#41;&nbsp;
-            </LinkButton>
+          <div>
+            <div className={styles.video_link}>
+              <Image
+                src="/images/project/2024_Othello.png"
+                alt='映像"Othello"'
+                width={960}
+                height={540}
+              />
+              <LinkButton link="https://x.com/3itsu8/status/1822951166063554767">
+                X&#40;Twitter&#41;&nbsp;
+              </LinkButton>
+            </div>
+            <section className={styles.content_info}>
+              <div className={styles.content_title}>
+                <h2>Othello</h2>
+              </div>
+              <div className={styles.content_caption}>
+                <div className={styles.content_credits}>
+                  <h3>Credits</h3>
+                  <p>映像 / みつば</p>
+                </div>
+                <div className={styles.content_about}>
+                  <h3>About</h3>
+                  <p>
+                    ソフトウェア / Blender, After Effects
+                    <br />
+                    オセロをモチーフにしたモーショングラフィクス作品です.
+                  </p>
+                </div>
+              </div>
+            </section>
           </div>
-          <section className={styles.content_info}>
-            <div className={styles.content_title}>
-              <h2>Othello</h2>
-            </div>
-            <div className={styles.content_caption}>
-              <div className={styles.content_credits}>
-                <h3>Credits</h3>
-                <p>映像 / みつば</p>
-              </div>
-              <div className={styles.content_about}>
-                <h3>About</h3>
-                <p>
-                  ソフトウェア / Blender, After Effects
-                  <br />
-                  オセロをモチーフにしたモーショングラフィクス作品です.
-                </p>
-              </div>
-            </div>
-          </section>
         </div>
       </section>
 
